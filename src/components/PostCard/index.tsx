30692c3f51326b770a4957adb149cda83c09a52a
ManuelRod <manuvski@gmail.com>
2023-03-13 20:47:51 +0100

    refactor:fixing merges

---
+++ src/components/PostCard/index.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+import { Container, Description, ImageContainer } from "./styles";
+import { Props } from "./types";
+
+const PostCard: FC<Props> = ({ onClick, title, image, comment }) => {
+  return (
+    <Container>
+      <ImageContainer src={image} />
+      <Description> {title} </Description>
+      <Description> {comment} </Description>
+    </Container>
+  );
+};
+
+export default PostCard;
